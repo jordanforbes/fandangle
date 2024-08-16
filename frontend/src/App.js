e90c662e8f0e76cb2ef3d879573f390c62a4a24b
Jordan Forbes <jordforb@gmail.com>
2024-08-16 10:47:59 -0400

    able to connect to rails backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/api/v1/movies")
+      .then((res) => console.log(res.data))
+      .catch((e) => console.error("api request failed: ", e));
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
